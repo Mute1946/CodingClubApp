6f3d29a728d8dacdbba040d5d4a8abef6aca06c4
Tim Sellmyer <tsellmyer@talonlpe.com>
2018-02-04 19:40:36 -0600

    Merge Update

---
+++ CodingClubApp/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingClubApp.Models
+{
+    public class Student
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string StudentID { get; set; }
+        public string PhoneNum { get; set; }
+        public string Email { get; set; }
+    }
+}
